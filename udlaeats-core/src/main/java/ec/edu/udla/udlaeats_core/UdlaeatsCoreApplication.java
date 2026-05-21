56b4263f4ed0bfd7183ea50739ca40dc9053e4b8
JohanKYT <kevin.maquis@udla.edu.ec>
2026-05-21 17:00:43 -0500

    Primer commit: Estructura base de UdlaEats - Back y Front

---
+++ udlaeats-core/src/main/java/ec/edu/udla/udlaeats_core/UdlaeatsCoreApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.udla.udlaeats_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdlaeatsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdlaeatsCoreApplication.class, args);
+	}
+
+}
---
+++ udlaeats-core/src/test/java/ec/edu/udla/udlaeats_core/UdlaeatsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.udla.udlaeats_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdlaeatsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
